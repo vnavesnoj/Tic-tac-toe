0d91fb22505c8eb2c42220da1c30ee6b73fe2e1f
vnavesnoj <vnavesnoj@gmail.com>
2022-06-29 12:22:53 +0300

    Add CommandLineArgumentParser component

--- src/main/java/vnavesnoj/tictactoe/component/GameFactory.java
+++ src/main/java/vnavesnoj/tictactoe/component/GameFactory.java
@@ -20,7 +20,6 @@ import vnavesnoj.tictactoe.component.keypad.DesktopNumericKeypadCellNumberConver
 import vnavesnoj.tictactoe.model.Player;
 import vnavesnoj.tictactoe.model.PlayerType;
 
-import static vnavesnoj.tictactoe.model.PlayerType.COMPUTER;
 import static vnavesnoj.tictactoe.model.PlayerType.USER;
 import static vnavesnoj.tictactoe.model.Sign.O;
 import static vnavesnoj.tictactoe.model.Sign.X;
@@ -36,31 +35,9 @@ public class GameFactory {
     private final PlayerType player2Type;
 
     public GameFactory(final String[] args) {
-        PlayerType player1Type = null;
-        PlayerType player2Type = null;
-        for (final String arg : args) {
-            if (USER.name().equalsIgnoreCase(arg) || COMPUTER.name().equalsIgnoreCase(arg)) {
-                if (player1Type == null) {
-                    player1Type = PlayerType.valueOf(arg.toUpperCase());
-                } else if (player2Type == null) {
-                    player2Type = PlayerType.valueOf(arg.toUpperCase());
-                } else {
-                    System.err.println("Unsupported command line argument: '" + arg + "'");
-                }
-            } else {
-                System.err.println("Unsupported command line argument: '" + arg + "'");
-            }
-        }
-        if (player1Type == null) {
-            this.player1Type = USER;
-            this.player2Type = COMPUTER;
-        } else if (player2Type == null) {
-            this.player1Type = USER;
-            this.player2Type = player1Type;
-        } else {
-            this.player1Type = player1Type;
-            this.player2Type = player2Type;
-        }
+        final CommandLineArgumentParser.PlayerTypes playerTypes = new CommandLineArgumentParser(args).parse();
+        this.player1Type = playerTypes.getPlayer1Type();
+        this.player2Type = playerTypes.getPlayer2Type();
     }
 
     public Game create() {
