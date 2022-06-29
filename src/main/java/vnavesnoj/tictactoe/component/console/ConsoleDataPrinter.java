8afa5aa0ca381696a2acef97040858215d7a9817
vnavesnoj <vnavesnoj@gmail.com>
2022-06-29 15:06:49 +0300

    Implement console user interface classes

---
+++ src/main/java/vnavesnoj/tictactoe/component/console/ConsoleDataPrinter.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2022. vnavesnoj <vnavesnoj@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package vnavesnoj.tictactoe.component.console;
+
+import vnavesnoj.tictactoe.component.CellNumberConverter;
+import vnavesnoj.tictactoe.component.DataPrinter;
+import vnavesnoj.tictactoe.model.Cell;
+import vnavesnoj.tictactoe.model.GameTable;
+
+/**
+ * @author vnavesnoj
+ * @link vnavesnoj@gmail.com
+ */
+public class ConsoleDataPrinter implements DataPrinter {
+
+    private final CellNumberConverter cellNumberConverter;
+
+    public ConsoleDataPrinter(final CellNumberConverter cellNumberConverter) {
+        this.cellNumberConverter = cellNumberConverter;
+    }
+
+    @Override
+    public void printInfoMessage(final String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public void printErrorMessage(final String message) {
+        System.err.println(message);
+    }
+
+    @Override
+    public void printMappingTable() {
+        print((i, k) -> String.valueOf(cellNumberConverter.toNumber(new Cell(i, k))));
+    }
+
+    @Override
+    public void printGameTable(final GameTable gameTable) {
+        print((i, k) -> String.valueOf(gameTable.getSign(new Cell(i, k))));
+    }
+
+    private void print(final Lambda lambda) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int k = 0; k < 3; k++) {
+                System.out.print("| " + lambda.getValue(i, k) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
+    }
+
+    @FunctionalInterface
+    private interface Lambda {
+
+        String getValue(int i, int k);
+    }
+}
---
+++ src/main/java/vnavesnoj/tictactoe/component/console/ConsoleUserInputReader.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2022. vnavesnoj <vnavesnoj@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package vnavesnoj.tictactoe.component.console;
+
+import vnavesnoj.tictactoe.component.CellNumberConverter;
+import vnavesnoj.tictactoe.component.DataPrinter;
+import vnavesnoj.tictactoe.component.UserInputReader;
+import vnavesnoj.tictactoe.model.Cell;
+
+import java.util.Scanner;
+
+/**
+ * @author vnavesnoj
+ * @link vnavesnoj@gmail.com
+ */
+public class ConsoleUserInputReader implements UserInputReader {
+
+    private final CellNumberConverter cellNumberConverter;
+
+    private final DataPrinter dataPrinter;
+
+    public ConsoleUserInputReader(final CellNumberConverter cellNumberConverter, final DataPrinter dataPrinter) {
+        this.cellNumberConverter = cellNumberConverter;
+        this.dataPrinter = dataPrinter;
+    }
+
+    @Override
+    public Cell getUserInput() {
+        dataPrinter.printInfoMessage("Please type number between 1 and 9: ");
+        final String enter = new Scanner(System.in).nextLine();
+        if (enter.length() == 1) {
+            if (enter.charAt(0) >= '1' && enter.charAt(0) <= '9') {
+                final int enteredNumber = Integer.parseInt(enter);
+                return cellNumberConverter.toCell(enteredNumber);
+            }
+        }
+        return null;
+    }
+}
