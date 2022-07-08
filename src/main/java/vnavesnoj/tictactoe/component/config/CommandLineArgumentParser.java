a35408d01812c5099c6af6934705e78dcd279537
vnavesnoj <vnavesnoj@gmail.com>
2022-07-08 19:26:04 +0300

    Set correct error messages for cmd arg parser

--- src/main/java/vnavesnoj/tictactoe/component/config/CommandLineArgumentParser.java
+++ src/main/java/vnavesnoj/tictactoe/component/config/CommandLineArgumentParser.java
@@ -46,16 +46,23 @@ public class CommandLineArgumentParser {
                 } else if (player2Type == null) {
                     player2Type = PlayerType.valueOf(arg.toUpperCase());
                 } else {
-                    System.err.println("Unsupported command line argument: '" + arg + "'");
+                    System.err.printf(
+                            "Invalid command line argument: '%s', because player types already set: " +
+                                    "player1Type = '%s', player2Type = '%s'%n",
+                            arg, player1Type, player2Type
+                    );
                 }
             } else if (UserInterface.CONSOLE.name().equalsIgnoreCase(arg) || UserInterface.GUI.name().equalsIgnoreCase(arg)) {
                 if (userInterface == null) {
                     userInterface = UserInterface.valueOf(arg.toUpperCase());
                 } else {
-                    System.err.println("Unsupported command line argument: '" + arg + "'");
+                    System.err.printf(
+                            "Invalid command line argument: '%s', because user interface already set: '%s'%n",
+                            arg, userInterface
+                    );
                 }
             } else {
-                System.err.println("Unsupported command line argument: '" + arg + "'");
+                System.err.printf("Unsupported command line argument: '%s'%n", arg);
             }
         }
         if (userInterface == null) {
